24a9100bf36243042c5792b22a6cf630f21d7f3d
K-ennethA <43216812+K-ennethA@users.noreply.github.com>
2023-10-18 19:04:56 -0700

    Add files via upload

---
+++ HUDTest/ActionMenu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/FightButton.grab_focus()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+
+func _on_fight_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_team_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_item_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_run_button_pressed():
+	pass # Replace with function body.
---
+++ HUDTest/action_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+#create actionbuttonnode
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	$MarginContainer/VBoxContainer/FightButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_fight_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_team_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_item_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_run_button_pressed():
+	pass # Replace with function body.
